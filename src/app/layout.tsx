5beb014220b324965af8b0b833e7f42a32f7e2d8
mynolog <minholee.log@gmail.com>
2025-03-22 22:00:37 +0900

    chore: HTML lang 속성을 'en' -> 'ko'로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <link rel="icon" href="/pavicon.png" />
         <link
